1c1b4b857a6a8e8c547e9d64a69e519c7ec83a7b
Valentyn Smaha <cfunsv10@gmail.com>
2022-01-05 17:14:37 +0200

    fix error handling

--- Sources/Upnp/Discovery/ProductIdentifier.swift
+++ Sources/Upnp/Discovery/ProductIdentifier.swift
@@ -6,7 +6,7 @@ struct ProductIdentifier {
         let name: String
         let version: Version
     }
-
+    
     let os: Token
     let upnp: Token
     let product: Token
@@ -16,9 +16,9 @@ extension ProductIdentifier: LosslessStringConvertible {
     init?(_ description: String) {
         let tokens = description.components(separatedBy: .whitespaces)
         guard tokens.count == 3,
-            let os = Token(tokens[0]),
-            let upnp = Token(tokens[1]),
-            let product = Token(tokens[2])
+              let os = Token(tokens[0]),
+              let upnp = Token(tokens[1]),
+              let product = Token(tokens[2])
         else {
             return nil
         }
@@ -26,25 +26,29 @@ extension ProductIdentifier: LosslessStringConvertible {
         self.upnp = upnp
         self.product = product
     }
-
+    
     var description: String { "\(os) \(upnp) \(product)"}
 }
 
 extension ProductIdentifier.Token: LosslessStringConvertible {
     init?(_ description: String) {
         let regex = try! NSRegularExpression(pattern: #"(\S+)\/(\S+)"#)
-
+        
         let searchRange = NSRange(description.startIndex..., in: description)
         guard let match = regex.firstMatch(in: description, range: searchRange) else { return nil }
-
+        
         guard let nameRange = Range(match.range(at: 1), in: description) else { return nil }
-
+        
         guard let versionRange = Range(match.range(at: 2), in: description) else { return nil }
-        guard let version = Version(String(description[versionRange])) else { return nil }
-
         self.name = String(description[nameRange])
-        self.version = version
+        
+        do {
+            try guard let version = Version(String(description[versionRange])) else { return nil }
+            self.version = version
+        } catch {
+            
+        }
     }
-
+    
     var description: String { "\(name)/\(version)" }
 }
